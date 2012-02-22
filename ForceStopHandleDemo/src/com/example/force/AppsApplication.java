d46d500fe506152919cb0d24f21cc650e287f25b
Rajiv Sharma <rajiv.sharma@rapidsoft.co.in>
2012-02-22 15:00:21 +0530

    added new project: Force Stop

---
+++ ForceStopHandleDemo/src/com/example/force/AppsApplication.java
@@ -0,0 +1,36 @@
+package com.example.force;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * 
+ * @author rapidsoft
+ * 
+ */
+
+public class AppsApplication extends Application implements ComponentCallbacks {
+
+
+	public AppsApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		Log.v("Application", "oncreate()");
+		super.onCreate();
+	}
+
+	@Override
+	public void onTerminate() {
+		// TODO Auto-generated method stub
+		Log.v("Application", "onTerminate()");
+		super.onTerminate();
+	}
+
+}
