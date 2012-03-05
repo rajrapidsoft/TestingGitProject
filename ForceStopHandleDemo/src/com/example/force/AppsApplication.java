2123b3bc822cbb93cf26aae29bba7968051b505c
Pooja Sharma <poojadaretodream@gmail.com>
2012-03-05 16:22:41 +0530

    chnages

--- ForceStopHandleDemo/src/com/example/force/AppsApplication.java
+++ ForceStopHandleDemo/src/com/example/force/AppsApplication.java
@@ -20,7 +20,7 @@ public class AppsApplication extends Application implements ComponentCallbacks {
 	public AppsApplication() {
 		// Changed made by Rajiv, on 05-02-2012 and the day was as monday....again I changed this line as the owner...changes...changes1..this is Rajiv
 		// One more line added----
-				// One more line added----added another word...added 2 end
+				// One more line added----added another word...added 2 end...............this change must from rajiv
 
 	}
 
